efdbcfa52d7745ce09f69578a3ab69b185ccb726
geli-gel <angele.zam@gmail.com>
2019-09-05 15:15:14 -0700

    added status method to Room and tests

--- lib/room.rb
+++ lib/room.rb
@@ -7,5 +7,26 @@ module Hotel
       @nightly_rate = 200
       @number = rooms_length + 1
     end
+
+    def status(desired_check_in:, desired_check_out:)
+      status = :AVAILABLE
+      @reservations.each do |reservation|
+        check_in_conflict = 
+          desired_check_in.between?(reservation.check_in_date, 
+                                    reservation.check_out_date - 1)
+        check_out_conflict = 
+          desired_check_out.between?(reservation.check_in_date + 1,
+                                     reservation.check_out_date)
+
+        if check_in_conflict || check_out_conflict
+          status = :UNAVAILABLE
+          break
+        end
+      end
+
+      return status
+
+    end
+
   end
 end
--- test/room_test.rb
+++ test/room_test.rb
@@ -13,4 +13,46 @@ describe "room class" do
     expect(@room_1.number).must_equal 1
   end
 
+  describe "status method" do
+    before do
+      @check_in = Date.today + 2
+      @check_out = Date.today + 4
+      @room_1.reservations << Hotel::Reservation.new(
+        check_in_date: @check_in,
+        check_out_date: @check_out,
+        room: @room_1
+        )
+    end
+    it "returns :UNAVAILABLE if desired date range is already reserved" do
+      status = @room_1.status(desired_check_in: @check_in,
+                              desired_check_out: @check_out
+                              )
+      expect(status).must_equal :UNAVAILABLE
+    end
+    
+    it "returns :AVAILABLE if desired_check_in == reserved check_out_date" do
+      status = @room_1.status(desired_check_in: @check_out,
+                              desired_check_out: @check_out + 3
+                              )
+      expect(status).must_equal :AVAILABLE
+    end
+
+    it "returns :AVAILABLE if desired_check_in == reserved check_out_date 
+      && desired_check_out == separate reserved check_in_date (between 2)" do
+      # Second Reservation
+      later_check_in = @check_out + 3
+      later_check_out = @check_out + 5
+      @room_1.reservations << Hotel::Reservation.new(
+        check_in_date: later_check_in,
+        check_out_date: later_check_out,
+        room: @room_1
+        )
+      # Status for dates in between the 2 reservations
+      status = @room_1.status(desired_check_in: @check_out,
+                              desired_check_out: later_check_in
+                              )
+      expect(status).must_equal :AVAILABLE
+    end
+  end
+
 end
