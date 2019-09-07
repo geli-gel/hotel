2ffe3cf0b604320738dda1f63a35e7853395abd3
geli-gel <angele.zam@gmail.com>
2019-09-07 00:09:09 -0700

    added @available_rooms and choose_room to Block

--- lib/block.rb
+++ lib/block.rb
@@ -1,12 +1,18 @@
 module Hotel
   class Block
-    attr_reader :id, :check_in_date, :check_out_date, :rooms
+    attr_reader :id, :check_in_date, :check_out_date, :rooms, :available_rooms
     def initialize(blocks_length:, check_in_date:, check_out_date:, rooms:, discount_rate:)
       @id = blocks_length + 1
       @check_in_date = check_in_date
       @check_out_date = check_out_date
       @rooms = rooms
       @discount_rate = discount_rate
+      @available_rooms = rooms
+    end
+
+    def choose_room
+      @available_rooms.shuffle!
+      return @available_rooms.pop
     end
 
   end
